1682d8b938aa6e4e87aae36ec0c06776efc0083c
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-09-21 13:09:09 +0300

    Create validParentesis.py

---
+++ validParentesis.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        c = 0
+        for i in range(len(s)):
+            if s[i] =="{":
+                st.append('}')
+                c+=1
+            elif s[i] =="[":
+                st.append(']')
+                c+=1
+            elif s[i] =="(":
+                st.append(')')
+                c+=1
+            elif len(st) > 0 and  s[i] == st[-1]:
+                st.pop()
+                c+=1
+            
+            
+       
+        return True if len(st) == 0 and c == len(s) else False
+            
