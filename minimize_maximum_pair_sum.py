40f09494a2d11f9311ffcccfd130375286c983f3
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-09-24 13:24:22 +0300

    Create minimize_maximum_pair_sum.py

---
+++ minimize_maximum_pair_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        l,r = 0,len(nums)-1
+        m = 0
+        while l<r:
+            m = max(m,(nums[l]+nums[r]))
+            l+=1
+            r-=1
+        return m
+            
