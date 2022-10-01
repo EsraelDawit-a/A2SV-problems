9b6dfaf54c31a738890958ce1a02518cbc6b2121
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-10-01 16:18:45 +0300

    Create power_of_three.py

---
+++ power_of_three.py
@@ -0,0 +1,23 @@
+from math import log
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <=0: 
+            return False
+        else:
+            return 3**(round(log(n,3))) == n
+       
+       
+
+
+       '''
+       or using recursion
+       '''
+
+       def is_power_of_three(n):
+           if n == 1:
+             return True
+           elif n < 3 or n%3 != 0 :
+              return False
+           else:
+             return is_power_of_three(n/3)
+              
