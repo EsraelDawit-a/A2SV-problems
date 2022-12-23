fb98cb34e997f8215da0fa67e2dd8c2b299a4a3c
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-12-23 22:43:14 +0300

    Time: 282 ms (88.69%), Space: 17 MB (78.59%) - LeetHub

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        x,y = len(grid)-1,len(grid[0])-1
+        
+        # pattern to find the sum
+        # s = grid[0][0]+grid[0][1]+grid[0][2]+grid[1][1]+grid[2][0]+grid[2][1]+grid[2][2]
+        max_sum = 0
+        for i in range(1,x):
+            for j in range(1,y):
+                top = (grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1])
+                middle = grid[i][j]
+                bottom = grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1]
+                
+                max_sum = max(max_sum,(top+middle+bottom))
+        
+        return max_sum
+                
+                
+            
