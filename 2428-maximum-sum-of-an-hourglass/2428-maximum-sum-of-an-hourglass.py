01ed33a223216dd64d90b6c1999e862807b161a5
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-12-24 08:44:26 +0300

    Update 2428-maximum-sum-of-an-hourglass.py

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -1,20 +1,25 @@
 class Solution:
     def maxSum(self, grid: List[List[int]]) -> int:
-        x,y = len(grid)-1,len(grid[0])-1
+        x,y = len(grid)-2,len(grid[0])-2
         
-        # Sliding window with Prefix sum
+        
         # pattern to find the sum
         # s = grid[0][0]+grid[0][1]+grid[0][2]+grid[1][1]+grid[2][0]+grid[2][1]+grid[2][2]
         max_sum = 0
-        for i in range(1,x):
-            for j in range(1,y):
-                top = (grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1])
-                middle = grid[i][j]
-                bottom = grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1]
+        # only check upto  len(grid)-1 for x and for y starting from index 1 len(grid)-1
+        # only check upto len(grid)-2 for x and y starting from index 0
+        for i in range(x):
+            for j in range(y):
+             
+                top = (grid[i][j]+grid[i][j+1]+grid[i][j+2])
+                middle = grid[i+1][j+1]
+                bottom = grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]
                 
                 max_sum = max(max_sum,(top+middle+bottom))
-        
+         
         return max_sum
                 
+                
+            
                 
             
