8df74eae5f42841bb58906d0e39d1b1807f460e8
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-12-24 09:08:36 +0300

    Time: 47 ms (92.14%), Space: 15.6 MB (18.81%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,8 +1,14 @@
+import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = "" 
-        for i in s:
-            if i.isalnum():
-                l+=i.lower()
-        return(l == l[::-1])
+        # replace only characters that are not number or alphabet [^a-z0-9]
+        s = re.sub("[^a-z0-9]", "", s.lower())
+        l,r = 0,len(s)-1
         
+        while l<r:
+            if s[l] !=s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+            
