1252cda1473f5b934f634a4a19e58e7b85430bc5
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2023-01-04 21:57:03 +0300

    Create power_mod_power.py

---
+++ power_mod_power.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import pow
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(int(pow(a,b)))
+print(int(pow(a,b)%m))
